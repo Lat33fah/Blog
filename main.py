a7a17449c5438cf34f521d28b4558552650fa718
Lateefah <lateefahajadi@gmail.com>
2022-07-29 17:56:03 +0100

    commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 posts = requests.get(url="https://api.npoint.io/cfe53b46bf48f290c306").json()
+my_email = os.environ['email']
+password = os.environ['password']
 app = Flask(__name__)
 
 
@@ -10,9 +15,20 @@ def home():
     return render_template('index.html', posts=posts)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template('contact.html')
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template('contact.html', msg_sent=True)
+    return render_template('contact.html', msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:Lateefah's Blog\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465) as connection:
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs='lateefahajadi@gmail.com', msg=email_message)
 
 
 @app.route("/about")
